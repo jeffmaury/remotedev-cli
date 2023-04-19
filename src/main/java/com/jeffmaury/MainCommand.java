f1a7b370860064988bea2de0c56ea8caf7d19013
Jeff MAURY <jmaury@redhat.com>
2023-04-19 14:50:06 +0200

    feat: add local command (#7)
    Signed-off-by: Jeff MAURY <jmaury@redhat.com>

--- src/main/java/com/jeffmaury/MainCommand.java
+++ src/main/java/com/jeffmaury/MainCommand.java
@@ -4,6 +4,6 @@ import io.quarkus.picocli.runtime.annotations.TopCommand;
 import picocli.CommandLine.Command;
 
 @TopCommand
-@Command(mixinStandardHelpOptions = true, subcommands = RemoteCommand.class)
+@Command(mixinStandardHelpOptions = true, subcommands = { LocalCommand.class, RemoteCommand.class })
 public class MainCommand {
 }
